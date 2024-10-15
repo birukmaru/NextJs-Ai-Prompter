51b0bdbb5a7ae0f9b9355519c7a64d120127d1af
Biruk Maru <birukyoni12@gmail.com>
2024-10-15 14:57:06 +0300

    Add the nav

--- next-ai-prompter/app/layout.jsx
+++ next-ai-prompter/app/layout.jsx
@@ -1,22 +1,28 @@
-import React, { children } from "react";
 import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
 };
 
-const RootLayout = ({ children }) => {
-  return (
-    <html lang="en">
-      <body>
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
         <div className="main">
           <div className="gradient" />
         </div>
-        <main className="app">{children}</main>
-      </body>
-    </html>
-  );
-};
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
